52935a50bcfca4986f37d77227ff4f3b515c4acb
Neeraj Gandharva <singh.neeraj0687@gmail.com>
2026-07-08 14:55:24 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+/* Active Menu */
+
+nav ul li a.active{
+
+    color:#58A6FF;
+
+    font-weight:600;
+
+}
+
+
+/* Scroll Animation */
+
+.hidden{
+
+    opacity:0;
+
+    transform:translateY(60px);
+
+    transition:all .8s ease;
+
+}
+
+.show{
+
+    opacity:1;
+
+    transform:translateY(0);
+
+}
